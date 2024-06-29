ad096c9e7c0ab6bc164013ef7d4c424f72304ddd
Renan Afonso Batista <rbatista@resutec.com.br>
2024-06-29 16:40:18 -0300

    add: ex00

---
+++ 07/ex00/include/whatever.hpp
@@ -0,0 +1,26 @@
+#ifndef _WHATEVER_HPP_
+#define _WHATEVER_HPP_
+
+#include <iostream>
+#include <string>
+
+#include "format.hpp"
+
+template <typename T>
+void swap(T &x, T &y) {
+    T tmp = x;
+    x = y;
+    y = tmp;
+}
+
+template <typename T>
+T const &min(T const &x, T const &y) {
+    return (x < y ? x : y);
+}
+
+template <typename T>
+T const &max(T const &x, T const &y) {
+    return (x > y ? x : y);
+}
+
+#endif
