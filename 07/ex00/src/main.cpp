56f690058240c21b48eda44afeca9224be64b5c1
Renan Batista <rbatista@resutec.com.br>
2024-07-03 18:58:26 +0000

    fix: ex00 main com mais testes e retirado arquivo não utilizado.

--- 07/ex00/src/main.cpp
+++ 07/ex00/src/main.cpp
@@ -1,17 +1,25 @@
 #include "whatever.hpp"
 
-int main(void) {
+int main(void)
+{
     int a = 2;
     int b = 3;
+    std::cout << "------> TEST START" << std::endl;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
     ::swap(a, b);
+    std::cout << "swap( a, b )" << std::endl;
     std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
-    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl << std::endl;
+
     std::string c = "chaine1";
     std::string d = "chaine2";
+    std::cout << "c = " << c << ", d = " << d << std::endl;
     ::swap(c, d);
+    std::cout << "swap( c, d )" << std::endl;
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
     std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+    std::cout << "------> TEST FINISH" << std::endl;
     return 0;
 }
